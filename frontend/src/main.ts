778c9d2a82e04cfdb957890361887f78d5ce5794
ChinhCDHE181478 <quansuper000@gmail.com>
2024-09-26 01:25:34 +0700

    create main class

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { HomeComponent } from './app/home/home.component'; 
 import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(HomeComponent, appConfig)
+  .catch(err => console.error(err));
