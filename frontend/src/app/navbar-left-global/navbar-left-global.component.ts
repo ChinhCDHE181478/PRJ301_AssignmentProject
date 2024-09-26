f53741c29b9c4be469cc8c85712ff1015cfa6d9d
ChinhCDHE181478 <quansuper000@gmail.com>
2024-09-26 11:28:07 +0700

    create backend file
    using dependencies: Spring web, spring data jpa, lombok, my sql driver, validation, spring security

---
+++ frontend/src/app/navbar-left-global/navbar-left-global.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-left-global',
+  standalone: true,
+  imports: [],
+  templateUrl: './navbar-left-global.component.html',
+  styleUrl: './navbar-left-global.component.scss'
+})
+export class NavbarLeftGlobalComponent {
+
+}
