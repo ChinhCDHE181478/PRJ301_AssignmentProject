64e0caf1baf9831d77d37cd2404dab4504280c54
ChinhCDHE181478 <quansuper000@gmail.com>
2024-09-27 09:48:06 +0700

    complete UI login

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,5 +5,5 @@ import { appConfig } from './app/app.config';
 import { HomeComponent } from './app/home/home.component'; 
 import { LoginComponent } from './app/login/login.component';
 
-bootstrapApplication(LoginComponent, appConfig)
+bootstrapApplication(HomeComponent, appConfig)
   .catch((err) => console.error(err));
